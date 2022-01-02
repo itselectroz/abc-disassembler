6318e231265f2022a73031f926fa7d481608560a
itselectroz <47079216+itselectroz@users.noreply.github.com>
2022-01-02 22:49:34 +0000

    Metadata info

---
+++ src/structures/item-info.ts
@@ -0,0 +1,7 @@
+import { u30 } from "../defined-transformer-types";
+import { Structure } from "../structure";
+
+export class ItemInfo extends Structure {
+    key: u30 = 0;
+    value: u30 = 0;
+}
---
+++ src/structures/metadata-info.ts
@@ -0,0 +1,8 @@
+import { custom, u30, vector } from "../defined-transformer-types";
+import { Structure } from "../structure";
+import { ItemInfo } from "./item-info";
+
+export class MetadataInfo extends Structure {
+    name: u30 = 0;
+    items: vector<custom<ItemInfo>, u30> = [];
+}
