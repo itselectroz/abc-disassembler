940363b3dfdd76248170bcdb64c0d7969f12578c
itselectroz <47079216+itselectroz@users.noreply.github.com>
2022-01-06 16:45:05 +0000

    Start of instruction list

---
+++ src/instructions/instruction-list.ts
@@ -0,0 +1,11 @@
+export const instructions = [
+    ["add", 0xA0],
+    ["add_i", 0xC5],
+    ["astype", 0x86, "multiname"],
+    ["astypelate", 0x87],
+    ["bitand", 0xA8],
+    ["bitnot", 0x97],
+    ["bitor", 0xA9],
+    ["bitxor", 0xAA],
+    ["call", 0x41, "u30"],
+]
